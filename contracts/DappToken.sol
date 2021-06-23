8eb0aa91e40c1706d307eb015a7b7cc03eb54c2a
Ali Sivandiy <59068329+alisivandiy@users.noreply.github.com>
2021-06-23 09:43:08 +0430

    First Commit

---
+++ contracts/DappToken.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.2 <0.9.0;
+contract DappToken {
+    string public name = "Dapp Token";
+    string public symbol = "DAPP";
+    string public standard = "DApp token v1.0";
+    uint256 public totalSupply;
+
+    // Transfer Event
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
+    // Approve Event
+    event Approval(
+        address indexed _owner,
+        address indexed _spender ,
+        uint256 _value
+    );
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    constructor (uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;     
+    }
+
+    // transfer Func
+    function transfer(address _to , uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender , _to , _value);
+        return true;
+    }
+
+    // approve func
+    function approve(address _spender , uint256 _value) public  returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender , _spender , _value);
+        return true;
+    }
+
+    function transferFrom(address _from , address _to , uint256 _value) public  returns (bool success) {
+        require(balanceOf[_from] >= _value);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from , _to , _value);
+        return true;
+    }
+}
